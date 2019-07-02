6ca6f81fa163e501192dd4b854ce49b16009b725
Dynamite <khaleelrahmon@gmail.comm>
2019-07-02 11:15:41 +0100

    Added Readme file

---
+++ images/NSFriends_files/site.js
@@ -0,0 +1,20 @@
+﻿(function () {
+
+	$("#theCarousel").carousel();
+
+var myImage = $("#theCarousel");
+
+var imageArray = ["images/Banner1.png","images/Banner2.png","images/Banner3.png"];
+var imageIndex = 0;
+
+function changeImage()  {
+	myImage.setAttribute("src",imageArray[imageIndex]);
+	imageIndex++;
+	if (imageIndex >= imageArray.length) {
+		imageIndex = 0;
+		}
+	}
+setInterval(changeImage,2000);
+
+
+})(); 
